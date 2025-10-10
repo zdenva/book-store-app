d56b1a48d0055bfef8172c2f73a68f5c7561eb3c
zdenva <vala.zdenek@proton.me>
2025-10-10 15:13:30 +0200

    test(be): add author router tests

---
+++ backend/tests/api/routes/test_authors.py
@@ -0,0 +1,56 @@
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from tests.utils.book.author import create_random_author
+
+
+def test_create_author(client: TestClient) -> None:
+    data = {"first_name": "Dave", "last_name": "Brown"}
+    response = client.post("authors/", json=data)
+
+    assert response.status_code == 200
+    content = response.json()
+    assert content["first_name"] == data["first_name"]
+    assert content["last_name"] == data["last_name"]
+    assert "id" in content
+
+
+def test_read_author(client: TestClient, db: Session) -> None:
+    author, *_ = create_random_author(db=db)
+    response = client.get(f"authors/{author.id}")
+    assert response.status_code == 200
+    content = response.json()
+    assert content["first_name"] == author.first_name
+    assert content["last_name"] == author.last_name
+    assert content["id"] == str(author.id)
+
+
+def test_read_authors(client: TestClient, db: Session) -> None:
+    create_random_author(db=db)
+    create_random_author(db=db)
+    response = client.get("authors/")
+    assert response.status_code == 200
+    content = response.json()
+    assert content["count"] >= 2
+
+
+def test_update_author(client: TestClient, db: Session) -> None:
+    author, *_ = create_random_author(db)
+    data = {"first_name": "Creg", "last_name": "Jones"}
+    response = client.patch(f"authors/{author.id}", json=data)
+    assert response.status_code == 200
+    content = response.json()
+    assert content["first_name"] == data["first_name"]
+    assert content["last_name"] == data["last_name"]
+    assert content["id"] == str(author.id)
+
+
+def test_delete_author(client: TestClient, db: Session) -> None:
+    author, *_ = create_random_author(db)
+    response = client.delete(f"authors/{author.id}")
+    assert response.status_code == 200
+    content = response.json()
+    assert (
+        content["message"]
+        == f"Author '{author.first_name} {author.last_name}' was successfully deleted"
+    )
